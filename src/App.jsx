30bc991b0ecbffaaa1ca970d7a8c7f3114f0ba7a
manosh <pun@thc.jpn.com>
2024-06-06 13:57:43 +0900

    add Next ui , tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import {Avatar} from "@nextui-org/react";
+
+function App() {
+
+  return (
+    <>
+      <div className="flex gap-3 items-center">
+        <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d"/>
+        <Avatar name="Junior"/>
+        <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026704d"/>
+        <Avatar name="Jane"/>
+        <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026702d"/>
+        <Avatar name="Joe"/>
+      </div>
+    </>
+  )
+}
+
+export default App
